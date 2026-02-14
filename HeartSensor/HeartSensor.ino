26e93c265e40e9792bbf8dd46b1228c90dcd02eb
Daniel Zheng <danielzj.zheng@mail.utoronto.ca>
2026-02-14 12:48:24 -0500

    initial commit

---
+++ HeartSensor/HeartSensor.ino
@@ -0,0 +1,45 @@
+
+/*  PulseSensor Starter Project and Signal Tester
+ *  The Best Way to Get Started  With, or See the Raw Signal of, your PulseSensor.com™ & Arduino.
+ *
+ *  Here is a link to the tutorial
+ *  https://pulsesensor.com/pages/code-and-guide
+ *
+ *  WATCH ME (Tutorial Video):
+ *  https://www.youtube.com/watch?v=RbB8NSRa5X4
+ *
+ *
+-------------------------------------------------------------
+1) This shows a live human Heartbeat Pulse.
+2) Live visualization in Arduino's Cool "Serial Plotter".
+3) Blink an LED on each Heartbeat.
+4) This is the direct Pulse Sensor's Signal.
+5) A great first-step in troubleshooting your circuit and connections.
+6) "Human-readable" code that is newbie friendly."
+
+*/
+
+
+//  Variables
+int PulseSensorPurplePin = 0;  // Pulse Sensor PURPLE WIRE connected to ANALOG PIN 0
+
+
+int Signal;           // holds the incoming raw data. Signal value can range from 0-1024
+int Threshold = 580;  // Determine which Signal to "count as a beat", and which to ingore.
+
+
+// The SetUp Function:
+void setup() {
+  Serial.begin(115200);  // Set's up Serial Communication at certain speed.
+}
+
+// The Main Loop Function
+void loop() {
+
+  Signal = analogRead(PulseSensorPurplePin);  // Read the PulseSensor's value.
+  // Assign this value to the "Signal" variable.
+
+  Serial.println("Signal " + String(Signal));  // Send "reading " followed by the Signal value to Serial Plotter.
+
+  delay(20);
+}
